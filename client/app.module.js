854d32083afabaa96ac538c56c82bd603a4cc003
hnrchrdl <hinrich@posteo.de>
2016-06-30 17:55:08 +0200

    separated client routes

--- client/app.module.js
+++ client/app.module.js
@@ -30,38 +30,8 @@ require('client/filter/timestring.filter')(module);
 
 
 /******** routes *********************************************/
-module.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise("/");
-
-    $stateProvider
-    .state('app', {
-        url: '/',
-        template: '<app></app>',
-        resolve: {
-            // authorizationCheck: ['$http', function($http) {
-            //     return $http.get()
-            //    // Do your API checks or whatever
-            // }],
-        }
-    })
-    .state('app.nowplaying', {
-        url: 'nowplaying',
-        template: '<nowplaying></nowplaying>',
-    })
-    .state('app.queue', {
-        url: 'queue',
-        template: '<queue></queue>',
-    })
-    .state('app.search', {
-        url: 'search/:input',
-        template: '<search></search>',
-    })
-    .state('app.artist', {
-        url: 'artist/:artist',
-        template: '<artist></artist>',
-    })
-}]);
+require('client/app.routes')(module);
 
 
 module.exports = module;
---
+++ client/app.routes.js
@@ -0,0 +1,40 @@
+module.exports = function(_module) {
+	
+	_module.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+	    .state('app', {
+	        url: '/',
+	        template: '<app></app>',
+	        resolve: {
+	            // authorizationCheck: ['$http', function($http) {
+	            //     return $http.get()
+	            //    // Do your API checks or whatever
+	            // }],
+	        }
+	    })
+	    .state('app.nowplaying', {
+	        url: 'nowplaying',
+	        template: '<nowplaying></nowplaying>',
+	    })
+	    .state('app.queue', {
+	        url: 'queue',
+	        template: '<queue></queue>',
+	    })
+	    .state('app.search', {
+	        url: 'search/:input',
+	        template: '<search></search>',
+	    })
+	    .state('app.artist', {
+	        url: 'artist/:artist',
+	        template: '<artist></artist>',
+	    })
+	    .state('app.settings', {
+	        url: 'settings',
+	        template: '<settings></settings>',
+	    });
+	}]);
+
+}
